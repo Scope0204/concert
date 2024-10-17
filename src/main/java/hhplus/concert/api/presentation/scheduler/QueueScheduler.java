82832551eb44b34937ad91f1f986a412e7f7bcf4
JunGyeongCho <scope0204@gmail.com>
2024-10-17 19:51:46 +0900

    feat: Queue 스케줄러 추가
    - c452de7 실제 스케줄러 로직 누락으로 인한 추가

---
+++ src/main/java/hhplus/concert/api/presentation/scheduler/QueueScheduler.java
@@ -0,0 +1,20 @@
+package hhplus.concert.api.presentation.scheduler;
+
+import hhplus.concert.application.queue.usecase.QueueFacade;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+@Component
+public class QueueScheduler {
+    private final QueueFacade queueFacade;
+
+    public QueueScheduler(QueueFacade queueFacade) {
+        this.queueFacade = queueFacade;
+    }
+
+    @Scheduled(fixedRate = 60000)
+    public void maintainActiveQueueCountWithScheduler() {
+        queueFacade.maintainActiveQueueCountWithScheduler();
+    }
+
+}
